7e524b361138d354739d1b7042d8d584f4978578
Jakub Bujak <jakbuj3@gmail.com>
2025-06-29 21:20:05 +0200

    creating web socket

--- server/server.js
+++ server/server.js
@@ -2,14 +2,47 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
+const { Server } = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+
 
 const Message = require('./models/Message');
 
 app.use(cors());
 app.use(express.json());
 
+
+const io = new Server(server, {
+    cors: {
+        origin: process.env.ANGULAR_APP_URL || 'http://localhost:4200',
+        methods: ['GET', 'POST']
+    }
+})
+
+io.on('connection', (socket) => {
+    console.log(`A user connected: ${socket.id}`);
+    socket.on('sendMessage', async (message) => {
+        const {username, content} = DataTransfer;
+        if (!username || !content){
+            console.warn('received invalid message:', message);
+            return;
+        }
+        try {
+            const newMessage = new Message({ username, content});
+            await newMessage.save();
+            io.emit('message', newMessage);
+            console.log(`Message saved and broadcasted: ${username}: ${content}`);
+        } catch (err) {
+            console.error('Error saving or broadcasting message:', err);
+        }
+    });
+    socket.on('disconnect', ()=> {
+        console.log(`User disconeccted: ${socket.id}`)
+    })
+})
 
 const mongoURI = process.env.MONGO_URI;
 
@@ -41,26 +74,27 @@ app.get('api/messages', async (req, resp) => {
         resp.status(500).json({error : "Server error"})
     }
 });
+
 //saving messages
-app.post('/api/messages', async (req, resp)=> {
-    const {username, content} = req.body;
-    if (!username || !content){
-        return resp.status(400).json({error : "Invalid username or message"});
-    };
-    try {
-        const newMessage = new Message({
-            username,
-            content
-        });
-        await newMessage.save();
-        resp.status(201).json(newMessage);
-    } catch (error) {
-        resp.status(500).json({error : "Server error"})
-    }
-})
+// app.post('/api/messages', async (req, resp)=> {
+//     const {username, content} = req.body;
+//     if (!username || !content){
+//         return resp.status(400).json({error : "Invalid username or message"});
+//     };
+//     try {
+//         const newMessage = new Message({
+//             username,
+//             content
+//         });
+//         await newMessage.save();
+//         resp.status(201).json(newMessage);
+//     } catch (error) {
+//         resp.status(500).json({error : "Server error"})
+//     }
+// })
 
 const PORT = process.env.PORT
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     run().catch(console.dir);
 });
